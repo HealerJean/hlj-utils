e3e6a4acb40a5185bbc874e757f47ef37b8ab2ff
HealerJean <healerjean@gmail.com>
2019-08-23 18:17:48 +0800

    变量

--- com-hlj-util/src/main/java/com/hlj/util/extend/D00变量.java
+++ com-hlj-util/src/main/java/com/hlj/util/extend/D00变量.java
@@ -5,6 +5,12 @@ package com.hlj.util.extend;
 class A{
 	// private String aString = "父亲变量";
 	public String aString = "父亲变量";
+
+
+	public void setaString(String aString){
+		this.aString = aString ;
+	}
+
 	public void aString(){
 		System.out.println("父亲方法");
 	}
@@ -13,17 +19,22 @@ public class D00变量 extends A{
 
 	public String aString = "儿子变量";
 
+
 	@Override
 	public void aString(){
 		aString = "方法儿子"; //没有改变string的值
 		System.out.println("儿子方法");
 	}
+
+
 	public static void main(String[] args) {
 		//多态
 		System.out.println("*****多态******");
-		A testB = new D00变量();
-		System.out.println(testB.aString ); //父亲变量
-		testB.aString();
+		A testA = new D00变量();
+		System.out.println(testA.aString ); //父亲变量
+		((D00变量) testA).setaString("setAstig"); //setAstig
+		System.out.println(testA.aString);
+		testA.aString();
 		System.out.println("***********");
 
 		D00变量 d00变量 = new D00变量();
