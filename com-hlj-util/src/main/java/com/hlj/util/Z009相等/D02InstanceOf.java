5205b10f1126c27ccf73af8632a8381accba1790
HealerJean <mxzdhealer@gmail.com>
2019-10-27 12:45:45 +0800

    dqueals

--- com-hlj-util/src/main/java/com/hlj/util/Z009相等/D02InstanceOf.java
+++ com-hlj-util/src/main/java/com/hlj/util/Z009相等/D02InstanceOf.java
@@ -2,6 +2,9 @@ package com.hlj.util.Z009相等;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author HealerJean
  * @version 1.0v
@@ -11,26 +14,26 @@ import org.junit.Test;
  */
 public class D02InstanceOf {
 
-    public  void instanceOf(Object object) {
+    public void instanceOf(Object object) {
 
         /**
          *  ==
          */
-        if(Long.class == object.getClass()){
+        if (Long.class == object.getClass()) {
             System.out.println("Long.class == object.getClass()");
         }
 
         /**
          * equals
          */
-        if(Long.class.equals(object.getClass()) ){
+        if (Long.class.equals(object.getClass())) {
             System.out.println("Long.class.equals(object.getClass()) ");
         }
 
         /**
          * instance Of
          */
-        if(object instanceof  Long){
+        if (object instanceof Long) {
             System.out.println("object instanceof  Long");
         }
     }
@@ -39,52 +42,66 @@ public class D02InstanceOf {
      * 测试类是否相当
      */
     @Test
-    public  void test相等() {
-        Long  object = 1100L;
+    public void test相等() {
+        Long object = 1100L;
         Integer va = 100;
-        instanceOf(object) ;
-        instanceOf(va) ;
+        instanceOf(object);
+        instanceOf(va);
     }
 
 
-class  Father{}
-class  Son extends  Father{}
+    class Father {
+    }
+
+    class Son extends Father {
+    }
 
 
-    public void fatherAndSon(){
+    public void fatherAndSon() {
         Father son = new Son(); //和 Son son = new Son(); 一样
         Father father = new Father();
 
         //son.getClass() ==  father.getClass() :false
-        System.out.println("son.getClass() ==  father.getClass() :" +(son.getClass() == father.getClass()) );
+        System.out.println("son.getClass() ==  father.getClass() :" + (son.getClass() == father.getClass()));
 
         //son.getClass().equals(father.getClass()) :false
-        System.out.println("son.getClass().equals(father.getClass()) :" +(son.getClass().equals(father.getClass())) );
+        System.out.println("son.getClass().equals(father.getClass()) :" + (son.getClass().equals(father.getClass())));
 
         //son  instanceof Father :true
-        System.out.println("son  instanceof Father :" +(son  instanceof Father ));
+        System.out.println("son  instanceof Father :" + (son instanceof Father));
 
         //father  instanceof Son :false   //父类不能  instanceof  子类
-        System.out.println("father  instanceof Son :" +(father  instanceof Son ));
+        System.out.println("father  instanceof Son :" + (father instanceof Son));
 
         //father  instanceof Father :true
-        System.out.println("father  instanceof Father :" +(father  instanceof Father ));
+        System.out.println("father  instanceof Father :" + (father instanceof Father));
 
         //son.getClass().isInstance(father):false  //子类不能 isInstance 父亲
-        System.out.println("son.getClass().isInstance(father):"+son.getClass().isInstance(father));
+        System.out.println("son.getClass().isInstance(father):" + son.getClass().isInstance(father));
 
         //father.getClass().isInstance(son):true
-        System.out.println("father.getClass().isInstance(son):"+father.getClass().isInstance(son));
+        System.out.println("father.getClass().isInstance(son):" + father.getClass().isInstance(son));
 
         //son.getClass().isInstance(son)true
-        System.out.println("son.getClass().isInstance(son)"+son.getClass().isInstance(son));
+        System.out.println("son.getClass().isInstance(son)" + son.getClass().isInstance(son));
 
     }
 
     @Test
-    public void testFatherAndSon(){
+    public void testFatherAndSon() {
         fatherAndSon();
     }
 
+
+    @Test
+    public void testInter() {
+        List<String> s = new ArrayList<>();
+
+        System.out.println(s instanceof List);
+
+        Object str =  s ;
+        System.out.println(str instanceof List);
+
+    }
 
 }
---
+++ com-hlj-util/src/main/java/com/hlj/util/Z015Poi/Z015Main.java
@@ -0,0 +1,114 @@
+package com.hlj.util.Z015Poi;
+
+import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author HealerJean
+ * @ClassName Z015Main
+ * @date 2019-08-19  21:47.
+ * @Description
+ */
+@Slf4j
+public class Z015Main {
+
+    /**
+     * 根据模板导出word文件
+     *
+     * @param map     ReportData对象为数据对象，里面存储Map 数据
+     * @param templateName   模板文件路径
+     * @param outputFileName 输出文件路径
+     */
+    public static void reportDoc( Map map, String templateName, String outputFileName) {
+        Map<String, Object> params = map;
+        InputStream in = null;
+        OutputStream outputStream = null;
+        IXDocReport report = null;
+
+        try {
+            // 1) Load ODT file and set Velocity template engine and cache it to the registry
+            in = new FileInputStream(new File(templateName));
+            report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Freemarker);
+
+            // 2) Create Java model context
+            IContext context = getReportContext(report, params);
+            // 输出文件，文件存在则删除
+            File outputFile = new File(outputFileName);
+            // 生成新的文件
+            outputStream = new FileOutputStream(outputFile);
+            report.process(context, outputStream);
+        } catch (IOException e) {
+            log.warn("文件流获取失败", e);
+        } catch (XDocReportException e) {
+            log.warn("导出失败", e);
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                log.warn("文件流关闭失败", e);
+            }
+        }
+    }
+    private static IContext getReportContext(IXDocReport report, Map<String, Object> params) throws XDocReportException {
+        IContext context = null;
+        if (report != null) {
+            context = report.createContext();
+            FieldsMetadata metadata = new FieldsMetadata();
+            for (Iterator iterator = params.entrySet().iterator(); iterator.hasNext(); ) {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                String name = entry.getKey().toString();
+                Object value = entry.getValue();
+                context.put(name, value);
+            }
+            report.setFieldsMetadata(metadata);
+        }
+        return context;
+    }
+
+
+
+    @Test
+    public void testXDocWord() throws Exception {
+        Map reportData = new HashMap();
+        // reportData.put("name", "张三");
+        // reportData.put("age", "2016-6-6");
+        //
+        // reportDoc(reportData, "/Users/healerjean/Desktop/template.docx", "/Users/healerjean/Desktop/t.docx");
+
+
+        InputStream in = new FileInputStream( new File("/Users/healerjean/Desktop/template.docx"));
+//设置模版类型
+        IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,TemplateEngineKind.Freemarker) ;
+//创建上下文
+        IContext context = report.createContext();
+//将将输出信息object放入上下文中
+        context.put("name", "张三");
+//OutputStream输出流，用以输出文件
+        OutputStream out = new FileOutputStream(new File("XXX.docx"));
+//将生成文件
+        report.process(context,out);
+        out.flush();
+
+
+    }
+
+
+
+
+}
